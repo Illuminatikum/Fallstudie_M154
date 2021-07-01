7358cb60c22b89e31813d29247170594257aa18b
Illuminatikum <leon.richli@bbzbl-it.ch>
2021-07-01 15:57:00 +0200

    Docker

---
+++ fitnesse_test/src/ch/ilv/fitnesse/Division.java
@@ -0,0 +1,19 @@
+package ch.ilv.fitnesse;
+
+import fit.ColumnFixture;
+
+public class Division extends ColumnFixture {
+  private double numerator, denominator;
+  
+  public void setNumerator(double numerator) {
+    this.numerator = numerator;
+  }
+  
+  public void setDenominator(double denominator) {
+    this.denominator = denominator;
+  }
+  
+  public double quotient() {
+    return numerator/denominator;
+  }
+} 
---
+++ test42/src/main/java/ilv/test42/AbstractLog.java
@@ -0,0 +1,22 @@
+package ilv.test42;
+
+/**
+ * Abstract base class for logger implementations
+ * @author mike
+ */
+public abstract class AbstractLog implements ILog {
+	protected int messageCount = 0;
+
+	@Override
+	public int getMessageCount() {
+		return messageCount;
+	}
+
+	/**
+	 * log current message count to log of implementing logger
+	 */
+	@Override
+	public void logMesssageCount() {
+		logMessage(messageCount + " message(s) logged");
+	}
+}
